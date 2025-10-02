cc7e60c92106194dc7fe4dea78a551fc392883d6
GavinSwartM <140915598+GavinSwartM@users.noreply.github.com>
2025-10-02 14:36:14 +0000

    Default Push

---
+++ main.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+const expect = std.testing.expect;
+
+const ROCKCOUNTER: u3 = 4;
+const ROCKSTRING: [4]u8 = [4]u8{ 'r', 'o', 'c', 'k' };
+const ROCKS: [4]u32 = [4]u32{ 1, 2, 3, 4 };
+
+fn rockFunction(numRocks: u32) bool {
+    return numRocks > ROCKCOUNTER;
+}
+
+test "if statement" {
+    defer std.debug.print("\n", .{});
+
+    for (ROCKS) |r| {
+        try expect(rockFunction(@as(u32, r)));
+    }
+}
